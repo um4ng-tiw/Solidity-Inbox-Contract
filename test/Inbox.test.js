59e8778f2fca1c502ccfaa70d408311a8ad9fafd
um4ng-squbix <umang.tiwari@squbix.com>
2021-12-26 18:40:51 +0530

    added contract deployment code

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,6 +2,7 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3"); //Constructor so capital. We will have to create an instance of Web3
+const { interface, bytecode } = require("../compile");
 
 // Here we create an instance of the Web3 library and
 // provide a ganache local network provider to the constructor
@@ -9,16 +10,23 @@ const Web3 = require("web3"); //Constructor so capital. We will have to create a
 const web3 = new Web3(ganache.provider());
 
 let fetchedAccounts;
+let inbox;
 
 beforeEach(async () => {
   // Getting list of all accounts from ganache
   fetchedAccounts = await web3.eth.getAccounts();
   // Use one of the accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hello world Umang"],
+    })
+    .send({ from: fetchedAccounts[0], gas: "1000000" });
 });
 
 describe("Inbox contract", () => {
   it("deploys a contract", () => {
-    console.log(fetchedAccounts);
+    console.log(inbox);
   });
 });
 
