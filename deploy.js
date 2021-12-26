696e716de332aaa0dada3904f339e4cbb4da202d
um4ng-squbix <umang.tiwari@squbix.com>
2021-12-26 20:03:25 +0530

    added env variable support and initial deployment code

--- deploy.js
+++ deploy.js
@@ -1,1 +1,13 @@
 // deploy code will go here
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const dotenv = require("dotenv").config();
+const { interface, bytecode } = require("./compile");
+
+// Takes mnemonic and url to network
+const provider = new HDWalletProvider(
+  process.env.MNEMONIC_PHRASE,
+  process.env.CHAIN_URL
+);
+
+const web3 = new Web3(provider);
